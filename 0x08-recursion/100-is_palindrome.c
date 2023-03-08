fe3d52889fa0838365bd8e4bfc65532559ec9a51
mikelade13 <mikaelade13>
2023-03-08 14:02:52 -0800

    wrote a function that returns 1 if a string is a palindrome and 0 if not.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,51 +1,50 @@
 #include "main.h"
 
 /**
- * is_prime_number - determine if a number is a prime number
- * @n: int number
- * Return: 1 if prime, 0 otherwise
+ * is_palindrome - check if a string is a palindrome
+ * @s: char array string
+ * Return: 1 if palindrome, 0 if not
  */
 
-int is_prime_number(int n)
+int is_palindrome(char *s)
 {
-	if (n < 2)
-		return (0);
-	if (n < 4)
-		return (1);
-	return (hai(n, 2));
+	int length;
+
+	length = get_length(s) - 1;
+
+	return (my_pal(s, --length));
 }
 
 /**
- * _sqrt - return square root of number
- * @x: number
- * @i: number incrementer acting as divisor
- * Return: square root of `x`
+ * get_length - gets length of string
+ * @s: string
+ * Return: return length of string
  */
 
-int _sqrt(int x, int i)
+int get_length(char *s)
 {
-	int square;
-
-	square = i * i;
-	if (square >= x)
-		return (i);
+	if (*s == '\0')
+		return (1);
 	else
-		return (_sqrt(x, i + 1));
+		return (1 + get_length(++s));
 }
 
 /**
- * hai - helper function, recursive steps taken
- * @n: number given to original function is_prime_number
- * @d: incrementer divisor
- * Return: 0 if not prime, 1 if prime
+ * my_pal - recursive check of palindrome
+ * @s: string
+ * @l: length of string
+ * Return: 1 if palindrome, 0 if not
  */
 
-int hai(int n, int d)
+int my_pal(char *s, int l)
 {
-	if (n % d == 0)
-		return (0);
-	else if (_sqrt(n, 1) < d)
-		return (1);
+	if (*s == *(s + l))
+	{
+		if (l <= 0)
+			return (1);
+		else
+			return (my_pal(++s, l - 2));
+	}
 	else
-		return (hai(n, d + 1));
-}
+		return (0);
+}}
